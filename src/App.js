611ffdc50db9d7f4bd9352f1842d098ce1759c8a
Leonardo Ferraz Novaes <leonardo.ferrazn@gmail.com>
2022-08-24 19:37:46 -0300

    feat: configurando rotas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Feed from './pages/Feed';
 
 function App() {
   return (
     <div >
-      
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/feed" element={<Feed />} />
+      </Routes>
     </div>
   );
 }
