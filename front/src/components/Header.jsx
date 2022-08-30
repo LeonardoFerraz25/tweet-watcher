7988d11dfd16bc9e4a0933688fb74135bbcb67f6
Leonardo Ferraz Novaes <leonardo.ferrazn@gmail.com>
2022-08-30 10:36:44 -0300

    feat: add botão de logout

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,5 +1,7 @@
-import { DotsThreeOutlineVertical, TwitterLogo } from 'phosphor-react';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { DotsThreeOutlineVertical, SignOut, TwitterLogo } from 'phosphor-react';
+import { Popover } from '@headlessui/react'
 
 export default function Header() {
   return (
@@ -15,7 +17,29 @@ export default function Header() {
         Tweet Watcher
       </h1>
       <div>
-        <DotsThreeOutlineVertical size={32} color="#1d9bf0" weight="fill" />
+      <Popover className="relative">
+        <Popover.Button>
+          <DotsThreeOutlineVertical size={32} color="#1d9bf0" weight="fill" />
+        </Popover.Button>
+
+        <Popover.Panel 
+          className="absolute right-1 top-12 bg-black p-2 rounded">
+          <div className="flex items-center">
+            <Link
+              to='/'
+              className='text-red-700 text-bold flex items-center'
+            >
+              Logout
+              <span
+                className='ml-2'
+              >
+                <SignOut size={32} color="#dc2626" weight="light" />
+              </span>
+            </Link>
+          </div>
+        </Popover.Panel>
+      </Popover>
+        
       </div>
     </header>
   );
